dc585e2c3ee143686b23d8c386501ac3f0b98734
zhangsan <312502865@qq.com>
2018-02-24 14:46:16 +0800

    code  submit

---
+++ src/main/java/com/zmx/util/MyConfig.java
@@ -0,0 +1,22 @@
+package com.zmx.util;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+//@Configuration
+//@EnableAutoConfiguration
+@PropertySource(value = "classpath:config.yml")
+@Component
+@ConfigurationProperties(prefix="test")
+
+
+public class MyConfig {
+    @Value("test.name")
+     public String name;
+
+}
